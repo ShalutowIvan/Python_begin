28683f1f8e06c5c9b9569c42cd1779c088c3bcd9
Иван <ivan_shalutow@mail.ru>
2022-06-23 00:05:20 +0700

    asd

--- FindFB.py
+++ FindFB.py
@@ -1,17 +1,34 @@
 #нужно сделать программу которая будет искать элементы списка в файле и выводить элементы строки выше
-file = open('123.txt', encoding='utf-8')
-s = file.readlines()
-a = 0
-z = ["безлунный", "широкой"]
-for i in range(len(s)):
-    for p in range(len(z)):
-        if z[p] in s[i]:
-            print(z[p])
-            print(s[i-2][17:])
+file = open('123.xml', encoding='utf-8')
+s = file.readlines() #присвоили содержимое файла баланса в переменную списка
+
+p = 0#это счетчик для вложенного цикла который ищет фбшки в строке
+j = 0
+z = list(map(str, input().split()))#это ввод списка алкокодов
+print()
+for i in range(len(s)):#перебираем переменную списка
+
+    for j in range(len(z)):
+        print(z[j])
+        while z[j] in s[i]:
+            #if z[p] in s[i]:
+            print(s[i][s[i].find("FB-", 0, -1):s[i].find("FB-", 0, -1)+18])
+            
+        else:
+            break
+            #нужно придумать условие для выхода из цикла
+        #p += 1
+    #p = 0
+
+            
+
+#фор наъодит только одну фбшку нужен цикл вайл, все фб перебрать нужно их может быть несколько
+
+#print(s[129][s[129].find("FB-"):s[129].find("FB-")+15]) это работает
 
 
 #print(a)
-
+#print(s[0][100])
 file.close()
 
 
