7404088440a0e55faa60810bdfaad504b324ebaa
Ivan <ivan_shalutow@mail.ru>
2022-07-25 00:51:20 +0700

    asd

---
+++ extension/FindFB.js
@@ -0,0 +1,26 @@
+let j = 0;
+let i = 0;
+//z = list(map(str, input("Введите алкокоды через пробел: ").split()))
+let iALC = 0;
+let iFB = 0;
+let countALC = 0;
+let sdvig = 0;
+
+let a = "asd asd asd asd";
+//console.log((a.match(/asd/) || []).length);
+console.log((a.split("asd").length - 1));//выводит колво вхождений подстроки в строку
+
+
+
+// for(var i = 0; i < len(z); i++){
+// countALC = bal.count(z[j])
+
+// for(var i = 0; i < countALC; i++){
+// iALC = bal.find(z[j], sdvig, -1)
+// iFB = bal.rfind("FB-", 0, iALC)
+// print(bal[iFB:iFB + 18])
+// sdvig = iALC + 25
+
+// }
+
+// }
