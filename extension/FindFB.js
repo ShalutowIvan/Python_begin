4b01ac6f267628e2e603a45a794431bfb80e6385
Иван <ivan_shalutow@mail.ru>
2022-07-26 19:02:36 +0700

    123

--- extension/FindFB.js
+++ extension/FindFB.js
@@ -8,19 +8,22 @@ let sdvig = 0;
 
 let a = "asd asd asd asd";
 //console.log((a.match(/asd/) || []).length);
-console.log((a.split("asd").length - 1));//выводит колво вхождений подстроки в строку
+
+
+let bal = ;//выводит колво вхождений подстроки в строку
+console.log(bal);
 
 
 
-// for(var i = 0; i < len(z); i++){
-// countALC = bal.count(z[j])
+for(var i = 0; i < len(z); i++){
+countALC = bal.split("asd").length - 1;
 
-// for(var i = 0; i < countALC; i++){
-// iALC = bal.find(z[j], sdvig, -1)
-// iFB = bal.rfind("FB-", 0, iALC)
-// print(bal[iFB:iFB + 18])
-// sdvig = iALC + 25
+for(var i = 0; i < countALC; i++){
+iALC = bal.find(z[j], sdvig, -1)
+iFB = bal.rfind("FB-", 0, iALC)
+print(bal[iFB:iFB + 18])
+sdvig = iALC + 25
 
-// }
+}
 
-// }
+}
