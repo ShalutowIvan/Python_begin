ae6323d6b5da72596813d0deba81056291d1b960
Ivan <ivan_shalutow@mail.ru>
2022-12-15 00:47:30 +0700

    ads

--- OOP.py
+++ OOP.py
@@ -2006,34 +2006,87 @@
 # Создайте в программе объект pt класса Point и еще один объект pt_clone через вызов метода clone.
 
 # P.S. В программе на экран ничего выводить не нужно.
-
+#мое решение
 # class Point:
 # 	def __init__(self, x, y):
 # 		self.x = x
 # 		self.y = y
-
-# 	def clone():
-# 		# a = Point(self.x, self.y)
-# 		# self.__dict__.copy()
-# 		# a = getattr(Point, x)
-# 		# b = getattr(Point, y)
-# 		# pt_clone = Point(a, b)
-# 		return self.__dict__.copy()
-# # сделать копию словаря
-
+#
+# 	def clone(self):
+# 		a = Point(self.x, self.y)
+# 		return a
+#
+#
 # pt = Point(55, 77)
-# print(id(pt))
-# print(pt.__dict__)
+# pt_clone = pt.clone()
+# у препода решение такое же как у меня
 
-# pt_clone = Point.clone
-# # print(id(pt_clone))
-# print(pt_clone)
+# сложное решение со словарями
+# class Point:
+# 	def __init__(self, x, y):
+# 		self.x = x
+# 		self.y = y
+#
+# 	def clone(self):
+# 		new_clone = super().__new__(type(self))
+# 		new_clone.__dict__.update(self.__dict__)
+# 		return new_clone
+#
+#
+# pt = Point(1, 2)
+# pt_clone = pt.clone()
 
-a = 5
-b = 5
+# Подвиг 10 (на повторение материала). В программе предполагается реализовать парсер (обработчик) строки (string) в определенный выходной формат. Для этого объявлен следующий класс:
+#
+# class Loader:
+#     def parse_format(self, string, factory):
+#         seq = factory.build_sequence()
+#         for sub in string.split(","):
+#             item = factory.build_number(sub)
+#             seq.append(item)
+#
+#         return seq
+#
+# И предполагается его использовать следующим образом:
+#
+# ld = Loader()
+# res = ld.parse_format("4, 5, -6.5", Factory())
+# На выходе (в переменной res) ожидается получить список из набора вещественных чисел. Например, для заданной строки, должно получиться:
+#
+# [4.0, 5.0, -6.5]
+#
+# Для реализации этой идеи необходимо вначале программы прописать класс Factory с двумя методами:
+#
+# build_sequence(self) - для создания начального пустого списка (метод должен возвращать пустой список);
+# build_number(self, string) - для преобразования переданной в метод строки (string) в вещественное значение (метод должен возвращать полученное вещественное число).
+#
+# Объявите класс с именем Factory, чтобы получать на выходе искомый результат.
+#
+# P.S. В программе на экран ничего выводить не нужно.
+#мое решение
+# class Factory:
+#     def build_sequence(self):
+#         return []
+#
+#     def build_number(self, string):
+#         return float(string)
+#
+# class Loader:
+#     def parse_format(self, string, factory):
+#         seq = factory.build_sequence()
+#         for sub in string.split(","):
+#             item = factory.build_number(sub)
+#             seq.append(item)
+#         return seq
+#
+# # эти строчки не менять!
+# ld = Loader()
+# # s = input()
+# s = "4, 5, -6.5"
+# res = ld.parse_format(s, Factory())
+# print(res)
 
-print()
-print(id(a))
-print(id(b))
 
-попробовать сделать так чтобы в при измененении объекта 5 он менялся во всех переменных
+# Методы класса (classmethod) и статические методы (staticmethod)!!!!!!!!!!!!!!!!!!
+
+
