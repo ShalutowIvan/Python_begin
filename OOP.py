6851bc4fd2852290d8cf682d6737fc3ac5c06ca8
Иван <ivan_shalutow@mail.ru>
2022-12-25 07:07:38 +0700

    asd

--- OOP.py
+++ OOP.py
@@ -3432,6 +3432,58 @@
 # P.S. На экран ничего выводить не нужно.
 
 
+class LinkedList:
+	def __init__(self):
+		self.head = None
+		self.tail = None
+		
+
+	def add_obj(self, obj):
+
+
+	def remove_obj(self):
+
+
+	def get_data(self):
+
+
+
+class ObjList:
+	def __init__(self, data):
+		self.__next = None
+		self.__prev = None
+		self.__data = data
+
+
+	def set_next(self, obj):
+
+
+
+	def set_prev(self, obj):
+
+
+
+	def get_next(self):
+
+
+	def get_prev(self):
+
+	def set_data(self, data):
+
+
+	def get_data(self):
+
+
+
+# lst = LinkedList()
+# lst.add_obj(ObjList("данные 1"))
+# lst.add_obj(ObjList("данные 2"))
+# lst.add_obj(ObjList("данные 3"))
+# res = lst.get_data()    # ['данные 1', 'данные 2', 'данные 3']
+
+
+
+
 
 
 
