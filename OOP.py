93cea1ecce454d4cec2a4762fd8fd553be14ee53
Иван <ivan_shalutow@mail.ru>
2023-02-15 19:53:11 +0700

    asd

--- OOP.py
+++ OOP.py
@@ -10288,6 +10288,7 @@ from string import ascii_lowercase, digits
 # __gt__() - для оператор больше или равно >=
 
 # Задачки!!!!!!!!!!
+
 # Подвиг 3. Объявите класс Track (маршрут), объекты которого создаются командой:
 
 # track = Track(start_x, start_y)
@@ -10390,12 +10391,151 @@ from string import ascii_lowercase, digits
 # track1 < track2  # True, если длина пути для track1 меньше, чем для track2
 
 
+# более ровное решение
+
+# class TrackLine:
+#     def __init__(self, to_x, to_y, max_speed):
+#         self.to_x = to_x
+#         self.to_y = to_y
+#         self.max_speed = max_speed
+
+
+# class Track:
+#     def __init__(self, start_x, start_y):
+#         self.start_x = start_x
+#         self.start_y = start_y
+#         self.track_lines = []
+
+#     def add_track(self, track):
+#         self.track_lines.append(track)
+
+#     def get_tracks(self):
+#         return tuple(self.track_lines)
+
+#     def __len__(self):
+#         ans = 0
+#         x1, y1 = self.start_x, self.start_y
+#         for obj in self.track_lines:
+#             x2, y2 = obj.to_x, obj.to_y
+#             ans += ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
+#             x1, y1 = x2, y2
+#         return int(ans)
+
+#     def __eq__(self, other):
+#         return len(self) == len(other)
+
+#     def __gt__(self, other):
+#         return len(self) > len(other)
+
+
+# track1 = Track(0, 0)
+# for d in [(2, 4), (5, -4)]: track1.add_track(TrackLine(*d, 100))
+
+# track2 = Track(0, 1)
+# for d in [(3, 2), (10, 8)]: track2.add_track(TrackLine(*d, 90))
+
+# res_eq = track1 == track2
+
+
+# Подвиг 4. Объявите класс Dimensions (габариты) с атрибутами:
+
+# MIN_DIMENSION = 10
+# MAX_DIMENSION = 10000
+# Каждый объект класса Dimensions должен создаваться командой:
+
+# d3 = Dimensions(a, b, c)   # a, b, c - габаритные размеры
+# Значения a, b, c должны сохраняться в локальных приватных атрибутах __a, __b, __c объектах этого класса.
+
+# Для изменения и доступа к приватным атрибутам в классе Dimensions должны быть объявлены объекты-свойства (property) с именами: a, b, c. Причем, в момент присваивания нового значения должна выполняться проверка попадания числа в диапазон [MIN_DIMENSION; MAX_DIMENSION]. Если число не попадает, то оно игнорируется и существующее значение не меняется.
+
+# С объектами класса Dimensions должны выполняться следующие операторы сравнения:
+
+# dim1 >= dim2   # True, если объем dim1 больше или равен объему dim2
+# dim1 > dim2    # True, если объем dim1 больше объема dim2
+# dim1 <= dim2   # True, если объем dim1 меньше или равен объему dim2
+# dim1 < dim2    # True, если объем dim1 меньше объема dim2
+# Объявите в программе еще один класс с именем ShopItem (товар), объекты которого создаются командой:
+
+# item = ShopItem(name, price, dim)
+# где name - название товара (строка); price - цена товара (целое или вещественное число); dim - габариты товара (объект класса Dimensions).
+
+# В каждом объекте класса ShopItem должны создаваться локальные атрибуты:
+
+# name - название товара;
+# price - цена товара;
+# dim - габариты товара (объект класса Dimensions).
+
+# Создайте список с именем lst_shop из четырех товаров со следующими данными:
+
+# - кеды; 1024; (40, 30, 120)
+# - зонт; 500.24; (10, 20, 50)
+# - холодильник; 40000; (2000, 600, 500)
+# - табуретка; 2000.99; (500, 200, 200)
+
+# Сформируйте новый список lst_shop_sorted с упорядоченными по возрастанию объема (габаритов) товаров списка lst_shop, используя стандартную функцию sorted() языка Python и ее параметр key для настройки сортировки. Прежний список lst_shop должен оставаться без изменений.
+
+# P.S. На экран в программе ничего выводить не нужно.
+
+# мое решение
+# четкий дескриптор:
+class Property:
+    def __set_name__(self, owner, name):
+        self.name = f'_{owner.__name__}__{name}'
+        self.min = owner.MIN_DIMENSION
+        self.max = owner.MAX_DIMENSION
+
+    def __get__(self, instance, owner):
+        if instance:        	
+            return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if self.min <= value <= self.max:
+            setattr(instance, self.name, value)
+
+
+class Dimensions:
+	MIN_DIMENSION = 10
+	MAX_DIMENSION = 10000
+	a = Property()
+	b = Property()
+	c = Property()
+
+	def __init__(self, a, b, c):
+		self.a = a
+		self.b = b
+		self.c = c
+
+	@classmethod
+	def __verify_data(cls, other):
+		if not isinstance(other, (int, Dimensions)):
+			raise TypeError("Операнд справа должен иметь тип int или Dimensions")
+		return other if isinstance(other, int) else other.
+
+
+	def __gt__(self, other):
+		V = 
+		return 
+
+# V=a*b*h формула расчета объема
+
+
+
+class ShopItem:
+	def __init__(self, name, price, dim):
+		self.name = name
+		self.price = price
+		self.dim = dim
 
 
 
 
 
 
+# dim1 >= dim2   # True, если объем dim1 больше или равен объему dim2
+# dim1 > dim2    # True, если объем dim1 больше объема dim2
+# dim1 <= dim2   # True, если объем dim1 меньше или равен объему dim2
+# dim1 < dim2    # True, если объем dim1 меньше объема dim2
 
 
+# lst_shop_sorted = 
 
