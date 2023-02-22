659f3b7b24c0fb20220687235c04c3fdb2497b11
Ivan <ivan_shalutow@mail.ru>
2023-02-23 00:05:14 +0700

    asd

--- OOP.py
+++ OOP.py
@@ -11396,6 +11396,80 @@ from string import ascii_lowercase, digits
 # h1, h2 = hash(r1), hash(r2)  # h1 == h2
 # print(h1 == h2)
 
+# Подвиг 6. Объявите класс с именем ShopItem (товар), объекты которого создаются командой:
+#
+# item = ShopItem(name, weight, price)
+# где name - название товара (строка); weight - вес товара (число: целое или вещественное); price - цена товара (число: целое или вещественное).
+#
+# Определите в этом классе магические методы:
+#
+# __hash__() - чтобы товары с одинаковым названием (без учета регистра), весом и ценой имели бы равные хэши;
+# __eq__() - чтобы объекты с одинаковыми хэшами были равны.
+#
+# Затем, из входного потока прочитайте строки командой:
+#
+# lst_in = list(map(str.strip, sys.stdin.readlines()))
+# Строки имеют следующий формат:
+#
+# название товара 1: вес_1 цена_1
+# ...
+# название товара N: вес_N цена_N
+#
+# Например:
+#
+# Системный блок: 1500 75890.56
+# Монитор Samsung: 2000 34000
+# Клавиатура: 200.44 545
+# Монитор Samsung: 2000 34000
+#
+# Как видите, товары в этом списке могут совпадать.
+#
+# Необходимо для всех этих строчек сформировать соответствующие объекты класса ShopItem и добавить в словарь с именем shop_items. Ключами словаря должны выступать сами объекты, а значениями - список в формате:
+#
+# [item, total]
+#
+# где item - объект класса ShopItem; total - общее количество одинаковых объектов (с одинаковыми хэшами). Подумайте, как эффективно программно наполнять такой словарь, проходя по списку lst_in один раз.
+#
+# P.S. На экран ничего выводить не нужно, только объявить класс и сформировать словарь.
+#
+# Sample Input:
+#
+# Системный блок: 1500 75890.56
+# Монитор Samsung: 2000 34000
+# Клавиатура: 200.44 545
+# Монитор Samsung: 2000 34000
+# Sample Output:
+
+lst_in = ['Системный блок: 1500 75890.56', 'Монитор Samsung: 2000 34000', 'Клавиатура: 200.44 545', 'Монитор Samsung: 2000 34000']
+class ShopItem:
+    def __init__(self, name, weight, price):
+        self.name = name
+        self.weight = weight
+        self.price = price
+    def __eq__(self, other):
+        return self.name == other.name and self.weight == other.weight and self.price == other.price
+    def __hash__(self):
+        return hash((self.name.lower(), self.weight, self.price))
+
+lst = map(lambda x: x.split(":"), lst_in)
+
+lst = list(map(lambda x: [x[0]] + x[1].split(), lst))
+# c = (len(lst) - len(set(lst_in))) + (len(lst) - len(set(lst_in)))
+c = 0
+shop_items = {}
+for i in lst:
+    if ShopItem(i[0], i[1], i[2]) in shop_items:
+        shop_items[ShopItem(i[0], i[1], i[2])] = [ShopItem(i[0], i[1], i[2]).name, c+2]
+    else:
+        shop_items[ShopItem(i[0], i[1], i[2])] = [ShopItem(i[0], i[1], i[2]).name, c]
+
+# lst_in = ['Системный блок: 1500 75890.56', 'Монитор Samsung: 2000 34000', 'Клавиатура: 200.44 545', 'Монитор Samsung: 2000 34000']
+# shop_items = {ShopItem(i[0], i[1], i[2]): [ShopItem(i[0], i[1], i[2]).name, c] for i in lst}
+
+print(shop_items.values())
+# print(c)
+
+
 
 
 
